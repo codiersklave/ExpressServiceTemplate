23b734db3797313924ae61b653df36034e2baa42
alexander.serbe <alexander.serbe@easy-tutor.eu>
2024-11-23 08:38:29 +0100

    Updated user create output

--- api/src/controllers/UserController.js
+++ api/src/controllers/UserController.js
@@ -6,7 +6,14 @@ export class UserController {
     try {
       const user = await userService.createUser(req.body);
       res.status(201).json({
+        id: user.id,
         email: user.email,
+        createdAt: user.createdAt,
+        createdBy: user.createdBy,
+        updatedAt: user.updatedAt,
+        updatedBy: user.updatedBy,
+        deletedAt: user.deletedAt,
+        deletedBy: user.deletedBy,
       });
     } catch (error) {
       if (error instanceof ValidationError) {
